3020a6c7112a5bc3f78797b4473b0e18f2d833eb
Clayrise <54859935+Clayrisee@users.noreply.github.com>
2020-10-29 16:49:31 +0700

    week-2: added appbar

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -8,6 +8,68 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            height: 100,
+            width: size.width,
+            padding: EdgeInsets.symmetric(
+              horizontal: 29,
+              vertical: 21,
+            ),
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 55,
+                  height: 55,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/profile.jpg"),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.circular(360.0)),
+                ),
+                SizedBox(
+                  width: 14,
+                ),
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Welcome!",
+                        style: kWhiteTextStyle.copyWith(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Kleyy",
+                        style: kWhiteTextStyle.copyWith(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
