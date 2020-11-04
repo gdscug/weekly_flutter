2716bbc7327d841b45afd685a5fe79ab7e9c77eb
Faiz Authar <faizauthar@gmail.com>
2020-11-04 20:51:36 +0700

    Home: make it real appbar

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -13,65 +13,69 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-            height: 100,
-            width: size.width,
-            margin: EdgeInsets.only(bottom: 15),
-            padding: EdgeInsets.symmetric(
-              horizontal: 29,
-              vertical: 21,
-            ),
-            decoration: BoxDecoration(
-              color: kPrimaryColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30),
-                bottomRight: Radius.circular(30),
-              ),
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 55,
-                  height: 55,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage("assets/profile.jpg"),
-                          fit: BoxFit.cover),
-                      borderRadius: BorderRadius.circular(360.0)),
-                ),
-                SizedBox(
-                  width: 14,
-                ),
-                Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Welcome!",
-                        style: kWhiteTextStyle.copyWith(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        widget.name,
-                        style: kWhiteTextStyle.copyWith(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 14.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+      appBar: AppBar(
+        elevation: 0,
+        toolbarHeight: 100,
+        leadingWidth: size.width,
+        backgroundColor: Colors.transparent,
+        title: Container(
+          margin: EdgeInsets.only(bottom: 15),
+          padding: EdgeInsets.symmetric(
+            horizontal: 29,
+            vertical: 21,
+          ),
+          decoration: BoxDecoration(
+            color: kPrimaryColor,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30),
             ),
           ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 55,
+                height: 55,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/profile.jpg"),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(360.0)),
+              ),
+              SizedBox(
+                width: 14,
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Welcome!",
+                      style: kWhiteTextStyle.copyWith(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      widget.name,
+                      style: kWhiteTextStyle.copyWith(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
           Container(
             margin: EdgeInsets.symmetric(horizontal: 10),
             child: Column(
